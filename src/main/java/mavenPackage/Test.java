872d2e46940bec0dd7b85d5690d37cee9626fa87
ASHISH  WAYACHAL <154004776+Ashishwayachal12@users.noreply.github.com>
2026-03-03 17:35:30 +0530

    Update greeting message in Test.java

--- src/main/java/mavenPackage/Test.java
+++ src/main/java/mavenPackage/Test.java
@@ -1,7 +1,7 @@
 package mavenPackage;
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello Buddy!");
+		System.out.println("Hello Buddy! This is a Test jenkins");
 
 	}
 
