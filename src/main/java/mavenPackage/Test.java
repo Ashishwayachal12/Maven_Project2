99bc5f05e35b0d1336d15a24ce8f38b032327294
ASHISH  WAYACHAL <154004776+Ashishwayachal12@users.noreply.github.com>
2026-03-05 08:59:49 +0530

    Refactor Test.java to simplify main method
    Removed ArrayList usage and list iteration logic.

--- src/main/java/mavenPackage/Test.java
+++ src/main/java/mavenPackage/Test.java
@@ -1,25 +1,6 @@
 package mavenPackage;
-import java.util.ArrayList;
-import java.util.ListIterator;
-
 public class Test {
-	public static void main(String[] args) {
-		ArrayList<Object> list = new ArrayList<Object>();
-		list.add("smith");
-		list.add("allen");
-		list.add("king");
-		list.add("kiran");
-		list.add("millan");
-		ListIterator<Object> listIterator = list.listIterator();
-		while (listIterator.hasNext()) {
-			System.out.println(listIterator.next());
-			System.out.println("---------------");
-		}
-		while (listIterator.hasPrevious()) {
-			System.out.println(listIterator.previous());
-		}
 		System.out.println("Hello Buddy! I am jenkins");
-
 	}
 
 }
